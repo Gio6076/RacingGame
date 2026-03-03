6abbcd96b44d8a9168c946cd1cbe6be48dab6588
Gio6076 <gebarola@gmail.com>
2026-03-03 22:52:48 +0800

    Implement PlayerController for character movement

---
+++ PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float acceleration;
+    public float turnSpeed;
+
+    private Rigidbody rb;
+    private float currentSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Vertical");
+        float turnInput = Input.GetAxis("Horizontal");
+
+
+        currentSpeed = Mathf.Lerp(currentSpeed, moveInput * maxSpeed, acceleration * Time.deltaTime);
+
+        Vector3 velocity = transform.forward * currentSpeed;
+        velocity.y = rb.velocity.y;
+
+        rb.velocity = velocity;
+
+        if (Mathf.Abs(currentSpeed) > 1f)
+        {
+            Quaternion turnRotation = Quaternion.Euler(0f, turnInput * turnSpeed * Time.fixedDeltaTime, 0f);
+            rb.MoveRotation(rb.rotation * turnRotation);
+        }
+
+        //Quaternion turn = Quaternion.Euler(0f, turnInput * turnSpeed * Time.fixedDeltaTime, 0f);
+        //rb.MoveRotation(rb.rotation * turn);
+    }
+}
